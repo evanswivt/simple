225d25a050c0f163dc4670550f56d6f7eb19051a
evan <evan@swivt.com.np>
2022-08-29 04:06:56 +0000

    esrw

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios'
 function App() {
+const data=  axios.get('https://jsonplaceholder.typicode.com/todos');
+console.log(data);
   return (
+   
    <h1>Welcome to Amplify website again</h1>
+   
   );
 }
+
 
 export default App;
